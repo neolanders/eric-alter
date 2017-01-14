ff737fcdec33231788ec53ca44fd7095335a483d
Eric Alter <neolanders@hotmail.com>
2017-01-13 22:26:16 -0400

    Nice intgration of ThreeJs

--- src/app/pages/playground/threejs/threejs.component.ts
+++ src/app/pages/playground/threejs/threejs.component.ts
@@ -1,17 +1,26 @@
-declare var THREE:any;
+// Threejs samples https://threejs.org/examples/#webgl_geometry_cube
 
-import { Component, OnInit } from '@angular/core';
+/// <reference path="../../../../../typings/globals/three/index.d.ts" />
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-threejs',
+  selector: 'app-threejs', // ps-scene
   templateUrl: './threejs.component.html',
   styleUrls: ['./threejs.component.scss']
 })
 export class ThreejsComponent implements OnInit {
-
-  constructor() { }
+  renderer: THREE.WebGLRenderer;
+  hostElement: ElementRef;
+  constructor(el:ElementRef) {
+    this.hostElement = el;
+  }
 
   ngOnInit() {
+    this.renderer = new THREE.WebGLRenderer({ alpha: true});
+    this.renderer.setSize(500,500);
+    this.renderer.setClearColor(0xFFAAAAFF,1);
+    this.hostElement.nativeElement.appendChild(this.renderer.domElement);
+    this.renderer.clear();
   }
 
 }
